64e701f7d1a1f9cf2be7130a0236f67e6e63b9c3
serhan.celik <serhan.celik@ozdilek.com.tr>
2025-06-16 16:59:16 +0300

    structure part 1

--- LinkseeApp/src/app/app.config.ts
+++ LinkseeApp/src/app/app.config.ts
@@ -1,8 +1,27 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
+import {definePreset} from '@primeng/themes';
+import material from '@primeng/themes/material';
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: definePreset(material, {
+            //Custom Options
+            }),
+            options: {
+                darkModeSelector: '.linksee-app-dark'
+            }
+        },
+    }),
+    provideStore()
+],
 };
